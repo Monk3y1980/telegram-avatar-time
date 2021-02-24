ac7df23ec0f96e26b498e929fe229676a71db6f4
Evgeny <49250650+Monk3y1980@users.noreply.github.com>
2021-02-24 06:55:55 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,5 +2,5 @@
 # https://my.telegram.org/auth
 # мы получаем оттуда две важные для нас вещи — это api_id и api_hash
 
-api_id = '3772283'
-api_hash = 'c1ee1474ad43724c9460f73217959add'
+api_id = '********'
+api_hash = '**********'
